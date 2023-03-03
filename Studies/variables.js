c87701a97d422fb686e7a4f42f4776924878815d
Jess Lee <khsy738@gmail.com>
2023-03-03 18:41:35 +0000

    Studies

---
+++ Studies/variables.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * VARIABLES:
+ * 
+ * 0: Variables are named containers for data in Javascript, You can declare a variable using 
+ * 3 different declarative keywords (var, let, const )
+ * 
+ */
